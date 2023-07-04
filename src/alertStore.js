6106e342a598dcf5d06da5c73ab84437ca64adf7
Roy Eriksen <roy.h.eriksen@gmail.com>
2023-07-04 16:52:01 +0800

    sign in and sign out branch

---
+++ src/alertStore.js
@@ -0,0 +1,15 @@
+import { writable } from 'svelte/store';
+
+export const alertStore = writable({ text: '', type: ''});
+
+export function showAlert(text, type) {
+    alertStore.set({ text, type });
+
+    setTimeout(() => {
+        clearAlert();
+      }, 3000);
+  }
+  
+  export function clearAlert() {
+    alertStore.set({ text: '', type: '' });
+  }
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,54 @@
-<h1 class="text-3xl font-bold underline">Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
-<button class="btn btn-success btn-outline">Button</button>
-<span class="badge">Badge</span>
+<script>
+    import { accessTokenStore, logout } from '../utils/auth.js';
+	import { onDestroy, onMount } from 'svelte';
+    import { writable } from 'svelte/store';
+    import { goto } from '$app/navigation';
+  
+
+    let isLoggedIn = writable(false);
+    let unsubscribe;
+    let showUploadForm = false // Track the visibility of the upload form
+
+    function updateLoginStatus(accessToken) {
+        isLoggedIn.set(!!accessToken);
+    }
+    
+    onDestroy(() => {
+        // Clean up the subscription when the component is destroyed
+        unsubscribe();
+    });
+
+    onMount(() => {
+        unsubscribe = accessTokenStore.subscribe(updateLoginStatus);
+        const storedAccessToken = localStorage.getItem('accessToken');
+        updateLoginStatus(storedAccessToken);
+        });
+        
+
+    async function handleLogout() {
+        logout();
+        isLoggedIn.set(false);
+        await goto('/');
+    }
+
+  </script>
+
+<style>
+    .header {
+      background-color: black;
+    }
+  </style>
+
+<header class="header flex justify-between items-center py-4 px-6">
+    <div class="logo text-white text-xl font-bold">Next Academy Project</div>
+    <div class="flex space-x-4">
+      {#if $isLoggedIn}
+      <button class="text-white" on:click={() => handleLogout()}>SIGN OUT</button>
+    {:else}
+      <button class="text-white" on:click|preventDefault={() => goto('/sign-up')}>BECOME A PROVIDER</button>
+      <button class="text-white" on:click|preventDefault={() => goto('/login')}>LOGIN</button>
+    {/if}    
+    </div>
+  </header>
+  
+  
