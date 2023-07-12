5ba4a6a23ce4d8a0112c3a5e2392b36ea1b01aa6
Tan Lee Ling <tanleeling98@gmail.com>
2023-07-12 15:34:25 +0800

    flowbite-user and provider signin signup

--- src/routes/user-login/+page.svelte
+++ src/routes/user-login/+page.svelte
@@ -16,6 +16,7 @@
   
   </script>
   
+
   <section class="bg-amber-400 dark:bg-gray-900">
     <div class="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
        
@@ -32,7 +33,7 @@
 
                 <form class="space-y-4 md:space-y-6" action="#">
                   <div>
-                      <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
+                      <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Email</label>
                       <input type="email" name="email" id="email" bind:value={email} class="bg-slate-300 border border-slate-600 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@gmail.com" required="">
                   </div>
 
