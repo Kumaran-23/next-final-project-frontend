744eb6cee0667f1a0c583ee1fff63bd1a0aeb3e3
“chelseahojx” <120448546+chelseahojx@users.noreply.github.com>
2023-07-14 01:39:54 +0800

    working on displaying availability

--- src/routes/provider/availability/+page.svelte
+++ src/routes/provider/availability/+page.svelte
@@ -1,17 +1,24 @@
 <script>
+  import { onMount } from "svelte";
   import { PUBLIC_BACKEND_BASE_URL } from "$env/static/public";
   import { getTokenFromLocalStorage } from "../../../utils/auth.js";
   import { time } from "../../../utils/time.js";
-  
-  let availability = [
-    { day: "Sun", startAt: "", endAt: "" },
-    { day: "Mon", startAt: "", endAt: "" },
-    { day: "Tue", startAt: "", endAt: "" },
-    { day: "Wed", startAt: "", endAt: "" },
-    { day: "Thu", startAt: "", endAt: "" },
-    { day: "Fri", startAt: "", endAt: "" },
-    { day: "Sat", startAt: "", endAt: "" },
-  ];
+
+  export let availability = null;
+  let isLoading = true;
+
+  onMount(() => {
+    availability = [
+      { day: "Sun", start_at: "", end_at: "" },
+      { day: "Mon", start_at: "", end_at: "" },
+      { day: "Tue", start_at: "", end_at: "" },
+      { day: "Wed", start_at: "", end_at: "" },
+      { day: "Thu", start_at: "", end_at: "" },
+      { day: "Fri", start_at: "", end_at: "" },
+      { day: "Sat", start_at: "", end_at: "" },
+    ];
+    isLoading = false;
+  });
   
   const handleTimeChange = (event, id) => {
     const { name, value } = event.target;
@@ -36,16 +43,16 @@
     let isValid = true;
 
     for (const item of availability) {
-      if (item.startAt && item.endAt) {
-        const startTime = parseInt(item.startAt);
-        const endTime = parseInt(item.endAt);
+      if (item.start_at && item.end_at) {
+        const startTime = parseInt(item.start_at);
+        const endTime = parseInt(item.end_at);
 
         if (endTime <= startTime) {
           alert("End time cannot be earlier than or equal to start time!");
           isValid = false;
           break;
         };
-      } else if (item.startAt || item.endAt) {
+      } else if (item.start_at || item.end_at) {
           alert("Both start time and end time must be filled out!");
           isValid = false;
           break;
@@ -54,7 +61,7 @@
 
     if (isValid) {
       const filteredAvailability = availability.filter(
-        (item) => item.startAt || item.endAt
+        (item) => item.start_at || item.end_at
       );
       console.log(filteredAvailability);
       handleCreateAvailability(filteredAvailability);
@@ -84,41 +91,45 @@
 <div class="container mx-auto px-24 py-12">
   <h1 class="text-center text-xl">Select your availability</h1>
 
-  {#each availability as item, id (id)}
-  <div class="grid grid-cols-3 gap-4" key={id}>
-    <div class="text-center my-4">
-      <p>{item.day}</p>
-    </div>
+  {#if isLoading}
+    <p>Loading availability...</p>
+  {:else}
+    {#each availability as item, id (id)}
+    <div class="grid grid-cols-3 gap-4" key={id}>
+      <div class="text-center my-4">
+        <p>{item.day}</p>
+      </div>
 
-    <div class="my-4">
-      <label class="label" for={`startAt-${id}`}>Start Time</label>
-      <select
-        name="startAt"
-        id={`startAt-${id}`}
-        class="select select-bordered w-full max-w-xs"
-        on:change={(event) => handleTimeChange(event, id)}
-      >
-      {#each time as t (t.id)}
-        <option value={t.t} id={t.id}>{t.t}</option>
-      {/each}
-      </select>
-    </div>
-
-    <div class="my-4">
-      <label class="label" for={`endAt-${id}`}>End Time</label>
-      <select
-        name="endAt"
-        id={`endAt-${id}`}
-        class="select select-bordered w-full max-w-xs"
-        on:change={(event) => handleTimeChange(event, id)}
-      >
+      <div class="my-4">
+        <label class="label" for={`start_at-${id}`}>Start Time</label>
+        <select
+          name="start_at"
+          id={`start_at-${id}`}
+          class="select select-bordered w-full max-w-xs"
+          on:change={(event) => handleTimeChange(event, id)}
+        >
         {#each time as t (t.id)}
-          <option value={t.t} id={t.id}>{t.t}</option>
+          <option value={t.t} id={t.id} selected={t.t === item.start_at}>{t.t}</option>
         {/each}
-      </select>
+        </select>
+      </div>
+
+      <div class="my-4">
+        <label class="label" for={`end_at-${id}`}>End Time</label>
+        <select
+          name="end_at"
+          id={`end_at-${id}`}
+          class="select select-bordered w-full max-w-xs"
+          on:change={(event) => handleTimeChange(event, id)}
+        >
+          {#each time as t (t.id)}
+            <option value={t.t} id={t.id} selected={t.t === item.end_at}>{t.t}</option>
+          {/each}
+        </select>
+      </div>
     </div>
-  </div>
-  {/each}
+    {/each}
+  {/if}
 
   <div class="flex justify-center items-center m-8">
     <button
